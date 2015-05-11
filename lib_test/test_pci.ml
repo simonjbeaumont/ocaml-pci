3fe4024e010061ad6ce86ba1b2acefa17d662714
Si Beaumont <simon.beaumont@citrix.com>
2015-05-11 18:15:45 +0100

    Grow test to be like `lspci -nnnDv`
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- lib_test/test_pci.ml
+++ lib_test/test_pci.ml
@@ -1,29 +1,47 @@
 open Pci
 
+(* This should be equivalent to `lspci -nnnDv` *)
 let _ =
   let pci_access = alloc () in
   init pci_access;
   scan_bus pci_access;
   let devs = Pci_access.devices pci_access in
   List.iter (fun d ->
-    let (_: int) = fill_info d [ FILL_IDENT; FILL_BASES; FILL_CLASS ] in
-    let c = read_byte d 0x3d in
+    let fill_flags = [ FILL_IDENT; FILL_CLASS; FILL_BASES; FILL_SIZES; FILL_PHYS_SLOT ] in
+    let (_: int) = fill_info d fill_flags in
     let open Pci_dev in
-    Printf.printf "%04x:%02x:%02x.%d vendor=%04x device=%04x class=%04x irq=%d (pin %d) base0=%nx"
-      (domain d) (bus d) (dev d) (func d) (vendor_id d) (device_id d)
-      (device_class d) (irq d) c (List.hd @@ base_addr d);
-    let name = lookup_device_name pci_access (vendor_id d) (device_id d) in
-    Printf.printf " (%s)\n" name
+    Printf.printf "Device: %04x:%02x:%02x.%d\n"
+      (domain d) (bus d) (dev d) (func d);
+    Printf.printf "Class:  %s [%04x]\n"
+      (lookup_class_name pci_access (device_class d)) (device_class d);
+    Printf.printf "Vendor: %s [%04x]\n"
+      (lookup_vendor_name pci_access (vendor_id d)) (vendor_id d);
+    Printf.printf "Device: %s [%04x]\n"
+      (lookup_device_name pci_access (vendor_id d) (device_id d)) (device_id d);
+    begin match subsystem_id d with
+    | Some (sv_id, sd_id) ->
+      Printf.printf "SVendor:\t%s [%04x]\n"
+        (lookup_vendor_name pci_access sv_id) sv_id;
+      Printf.printf "SDevice:\t%s [%04x]\n"
+        (lookup_subsystem_name pci_access (vendor_id d) (device_id d) sv_id sd_id) sd_id
+    | None -> ()
+    end;
+    begin match phy_slot d with
+    | Some slot -> Printf.printf "PhySlot:\t%s\n" slot
+    | None -> ()
+    end;
+    print_endline ""
   ) devs;
 
-  match devs with
+  begin match devs with
   | [] -> ()
   | d::ds ->
-      let open Pci_dev in
-      Printf.printf "Getting region sizes for device %04x:%02x:%02x.%d\n"
-        (domain d) (bus d) (dev d) (func d);
-      List.iteri (fun i size ->
-        Printf.printf "\tRegion %d has size %nd\n" i size
-      ) (size d);
+    let open Pci_dev in
+    Printf.printf "Getting region sizes for device %04x:%02x:%02x.%d\n"
+      (domain d) (bus d) (dev d) (func d);
+    List.iteri (fun i size ->
+      Printf.printf "\tRegion %d has size %nd\n" i size
+    ) (size d)
+  end;
 
   cleanup pci_access
