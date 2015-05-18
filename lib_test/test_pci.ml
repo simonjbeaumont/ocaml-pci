96b6c4d007c9dca323a3a592dee0f56e1096b6b4
Si Beaumont <simon.beaumont@citrix.com>
2015-05-18 15:45:57 +0100

    Measure resident memory for leak via /proc/[pid]/statm
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- lib_test/test_pci.ml
+++ lib_test/test_pci.ml
@@ -1,35 +1,30 @@
 open OUnit
 open Pci
 
-let vm_size () =
-  let stream_of_channel ic =
-    Stream.from (fun _ -> try Some (input_line ic) with End_of_file -> None) in
-  let smaps_path = List.fold_left Filename.concat "/"
-    [ "proc"; Unix.getpid () |> string_of_int; "status" ] in
-  let rec list_of_stream acc s =
-    try list_of_stream (Stream.next s::acc) s
-    with Stream.Failure -> List.rev acc in
-  let lines = open_in smaps_path |> stream_of_channel |> list_of_stream [] in
-  let size_line = List.find (fun line -> Str.(string_match (regexp "VmSize:") line 0)) lines in
-  let size_kb = List.nth (Str.(split (regexp "\ +")) size_line) 1 in
-  int_of_string size_kb
-
+let resident_pages () =
+  let with_channel c f =
+    try let r = f c in close_in c; r
+    with exn -> close_in_noerr c; raise exn in
+  let statm_path = List.fold_left Filename.concat "/"
+    [ "proc"; Unix.getpid () |> string_of_int; "statm" ] in
+  let statm = with_channel (open_in statm_path) input_line in
+  Scanf.sscanf statm "%d %d %d %d %d %d %d" (fun _ res _ _ _ _ _ -> res)
 
 let smoke_test () =
   with_access (fun a -> let (_: Pci_dev.t list) = get_devices a in ())
 
 let test_with_access_cleanup () =
   (* Get overhead for calling the fuction and the measuremnt functions *)
-  let _ = Gc.compact (); vm_size () in
+  let _ = Gc.compact (); resident_pages () in
   for i = 1 to 1000 do with_access ~cleanup:true (fun _ -> ()) done;
-  let mem = Gc.compact (); vm_size () in
+  let mem = Gc.compact (); resident_pages () in
   (* The incremental cost of calling with_access should be 0 *)
   for i = 1 to 1000 do with_access ~cleanup:true (fun _ -> ()) done;
-  let mem' = Gc.compact (); vm_size () in
-  assert_equal ~printer:(Printf.sprintf "VmSize = %d kB") mem mem';
+  let mem' = Gc.compact (); resident_pages () in
+  assert_equal ~printer:(Printf.sprintf "VmRSS = %d pages") mem mem';
   (* Checking for a difference with cleanup=false as a negative test *)
   for i = 1 to 1000 do with_access ~cleanup:false (fun _ -> ()) done;
-  let mem'' = Gc.compact (); vm_size () in
+  let mem'' = Gc.compact (); resident_pages () in
   assert_raises (OUnitTest.OUnit_failure "not equal") (fun () ->
     assert_equal mem' mem'')
 
