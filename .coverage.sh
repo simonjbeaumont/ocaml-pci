a2d2a6cc66eda54bfb706fbdd396da4f59d795e9
Si Beaumont <simon.beaumont@citrix.com>
2015-05-25 15:09:51 +0100

    Run ocveralls to upload coverage data to coveralls
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- .coverage.sh
+++ .coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 eval `opam config env`
-opam install -y bisect_ppx oasis
+opam install -y bisect_ppx oasis ocveralls
 
 sed '/BuildDepends:/ s/$/, bisect_ppx/' _oasis > _oasis_with_bisect
 oasis setup -oasis _oasis_with_bisect
@@ -9,4 +9,7 @@ oasis setup -oasis _oasis_with_bisect
 ./configure --enable-tests
 make
 
+rm -f _build/bisect*
 BISECT_FILE=_build/bisect ./test_pci.native
+
+ocveralls --prefix _build _build/bisect* --send
