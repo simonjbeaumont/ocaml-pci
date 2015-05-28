fec9798d7cde05b5cc97e60f76c7ba9e59054a4c
Si Beaumont <simon.beaumont@citrix.com>
2015-05-28 11:47:13 +0100

    Update gh-pages with coverage
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- .coverage.sh
+++ .coverage.sh
@@ -25,6 +25,19 @@ find . -name bisect* | xargs rm -f
 if [ -n "$TRAVIS" ]; then
   echo "\$TRAVIS set; running ocveralls and sending to coveralls.io..."
   ocveralls --prefix _build bisect* --send
+
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "This is not a pull-request, not pushing report to gh-pages..."
+    exit 0
+  fi
+  GHPDIR=gh-pages
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/simonjbeaumont/ocaml-pci $GHPDIR > /dev/null
+  (cd _build; bisect-report ../bisect*.out -html ../$GHPDIR/coverage)
+
+  git -C $GHPDIR config user.email "travis@travis-ci.org"
+  git -C $GHPDIR config user.name "Travis"
+  git -C $GHPDIR commit --allow-empty -am "Travis build $TRAVIS_BUILD_NUMBER pushed coverage to gh-pages"
+  git -C $GHPDIR push origin gh-pages > /dev/null
 else
   echo "\$TRAVIS not set; running bisect-report..."
   bisect-report bisect*.out -I _build -text report
