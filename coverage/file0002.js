1bcabdf00103f41e03fb659ba055fd5513eb7e24
Travis <travis@travis-ci.org>
2015-05-28 15:35:48 +0000

    Travis build 63 pushed coverage to gh-pages

--- coverage/file0002.js
+++ coverage/file0002.js
@@ -1,18 +1,14 @@
 var texts = new Array();
 var states = new Array();
 
-texts['fold000001'] = '<a href="javascript:fold(\'fold000001\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 1 to line 7</i>';
+texts['fold000001'] = '<a href="javascript:fold(\'fold000001\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 1 to line 9</i>';
 states['fold000001'] = false;
-texts['fold000013'] = '<a href="javascript:fold(\'fold000013\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 13 to line 14</i>';
-states['fold000013'] = false;
-texts['fold000016'] = '<a href="javascript:fold(\'fold000016\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 16 to line 18</i>';
+texts['fold000011'] = '<a href="javascript:fold(\'fold000011\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 11 to line 14</i>';
+states['fold000011'] = false;
+texts['fold000016'] = '<a href="javascript:fold(\'fold000016\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 16 to line 38</i>';
 states['fold000016'] = false;
-texts['fold000022'] = '<a href="javascript:fold(\'fold000022\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 22 to line 22</i>';
-states['fold000022'] = false;
-texts['fold000026'] = '<a href="javascript:fold(\'fold000026\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 26 to line 26</i>';
-states['fold000026'] = false;
-texts['fold000030'] = '<a href="javascript:fold(\'fold000030\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 30 to line 58</i>';
-states['fold000030'] = false;
+texts['fold000048'] = '<a href="javascript:fold(\'fold000048\');"><img border="0" height="10" width="10" src="plus.png" title="unfold code"/></a><i>&nbsp;&nbsp;code folded from line 48 to line 58</i>';
+states['fold000048'] = false;
 
 function fold(id) {
   tmp = document.getElementById(id).innerHTML;
