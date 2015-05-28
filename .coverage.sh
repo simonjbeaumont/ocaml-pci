65fd42623f6df4878f4be481adff7ff95fc733c7
Si Beaumont <simon.beaumont@citrix.com>
2015-05-28 12:26:26 +0100

    Add badge to gh-pages

--- .coverage.sh
+++ .coverage.sh
@@ -22,10 +22,13 @@ make
 find . -name bisect* | xargs rm -f
 ./test_pci.native
 
-if [ -n "$TRAVIS" ]; then
-  echo "\$TRAVIS set; running ocveralls and sending to coveralls.io..."
-  ocveralls --prefix _build bisect* --send
+bisect-report bisect*.out -I _build -text report
+bisect-report bisect*.out -I _build -summary-only -text summary
+(cd _build; bisect-report ../bisect*.out -html ../report-html)
 
+if [ -n "$TRAVIS" ]; then
+  # echo "\$TRAVIS set; running ocveralls and sending to coveralls.io..."
+  # ocveralls --prefix _build bisect* --send
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "This is not a pull-request, not pushing report to gh-pages..."
     exit 0
@@ -34,15 +37,24 @@ if [ -n "$TRAVIS" ]; then
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/simonjbeaumont/ocaml-pci $GHPDIR > /dev/null
   (cd _build; bisect-report ../bisect*.out -html ../$GHPDIR/coverage)
 
+  PERCENTAGE=$(grep total summary | awk '{print $4}' | tr -d "()%" | cut -d. -f1)
+
+  if   [ "$PERCENTAGE" -ge 95 ]; then COLOR="brightgreen"
+  elif [ "$PERCENTAGE" -ge 85 ]; then COLOR="green"
+  elif [ "$PERCENTAGE" -ge 75 ]; then COLOR="yellowgreen"
+  elif [ "$PERCENTAGE" -ge 65 ]; then COLOR="yellow"
+  elif [ "$PERCENTAGE" -ge 50 ]; then COLOR="orange"
+  elif [ "$PERCENTAGE" -ge 00 ]; then COLOR="red"
+  else                                COLOR="lightgrey"; PERCENTAGE="unknown"
+  fi
+  echo "<img src=\"http://img.shields.io/badge/coverage-$PERCENTAGE%-$COLOR\"/>" > $GHPDIR/coverage/badge.html
+
   git -C $GHPDIR config user.email "travis@travis-ci.org"
   git -C $GHPDIR config user.name "Travis"
   git -C $GHPDIR commit --allow-empty -am "Travis build $TRAVIS_BUILD_NUMBER pushed coverage to gh-pages"
   git -C $GHPDIR push origin gh-pages > /dev/null
 else
-  echo "\$TRAVIS not set; running bisect-report..."
-  bisect-report bisect*.out -I _build -text report
-  bisect-report bisect*.out -I _build -summary-only -text summary
-  (cd _build; bisect-report ../bisect*.out -html ../report-html)
+  echo "\$TRAVIS not set; displaying results of bisect-report..."
   cat report
   cat summary
 fi
