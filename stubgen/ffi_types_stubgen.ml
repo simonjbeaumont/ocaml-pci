4d9c427f5e66c189e6c0cad534a3da7cf93a3889
Christian Lindig <christian.lindig@citrix.com>
2021-03-02 10:45:11 +0000

    Reformat with OCamlformat
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- stubgen/ffi_types_stubgen.ml
+++ stubgen/ffi_types_stubgen.ml
@@ -1,3 +1,4 @@
 let () =
-  print_endline "#include <pci/pci.h>";
-  Cstubs.Types.write_c Format.std_formatter (module Pci_bindings.Ffi_bindings.Types)
+  print_endline "#include <pci/pci.h>" ;
+  Cstubs.Types.write_c Format.std_formatter
+    (module Pci_bindings.Ffi_bindings.Types)
